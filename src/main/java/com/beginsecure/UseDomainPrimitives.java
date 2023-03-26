460644f8e1906974a5e36d0e5f99e22d37a7fc97
Oleg <Oleg-BLR@yandex.ru>
2023-03-27 00:18:01 +0300

    second commit

--- src/main/java/com/beginsecure/UseDomainPrimitives.java
+++ src/main/java/com/beginsecure/UseDomainPrimitives.java
@@ -1,2 +1,17 @@
-package com.beginsecure;public class UseDomainPrimitives {
+package com.beginsecure;
+
+import com.beginsecure.domain.primitives.SSN;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UseDomainPrimitives {
+    public static void main(String[] args) {
+        List<SSN> ids = new ArrayList<>();
+
+        ids.add(new SSN("019-85-6636"));
+        ids.add(new SSN("312-85-2646"));
+
+        ids.add(new SSN("312-88-2646"));
+    }
 }
